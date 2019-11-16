fc0c895cd5895b64885352521cfe17351d758a0f
www <l2u0k0a0s.ld@gmail.com>
2019-11-16 01:57:10 +0100

    Update to 000.4 [includes redesign]

--- pwgen_pre.py
+++ pwgen_pre.py
@@ -1,58 +1,59 @@
 # -*- coding: utf-8 -*-
-from PyQt5.QtWidgets import QMainWindow
-from PyQt5.QtWidgets import QApplication, QWidget
-from PyQt5 import QtCore, QtWidgets, QtGui
+
 import random
 import string
 import sys
 
+from PyQt5 import QtCore, QtWidgets, QtGui
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QMainWindow
+
+
+def exitP():
+    sys.exit(app.exec_())
+
 
 class Ui_MainWindow(QWidget):
+
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.setFixedSize(220, 280)
+        MainWindow.setFixedSize(680, 480)
         MainWindow.setStyleSheet("background: rgb(25, 25, 25)")
-        MainWindow.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+        MainWindow.setWindowIcon(QIcon('logo.png'))
+        MainWindow.setWindowFlags(QtCore.Qt.WindowCloseButtonHint)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.button_gen = QtWidgets.QPushButton(self.centralwidget)
-        self.button_gen.setGeometry(QtCore.QRect(20, 90, 75, 20))
+        self.button_gen.setGeometry(QtCore.QRect(20, 65, 80, 20))
         self.button_gen.clicked.connect(self.generatePW)
         self.button_gen.setObjectName("button_gen")
         self.button_gen.setAutoFillBackground(False)
         self.button_gen.setStyleSheet("background: rgb(186, 186, 186)")
         self.label_pw = QtWidgets.QLabel(self.centralwidget)
-        self.label_pw.setGeometry(QtCore.QRect(20, 20, 181, 31))
+        self.label_pw.setGeometry(QtCore.QRect(20, 20, 640, 30))
         self.label_pw.setFrameShape(QtWidgets.QFrame.Box)
         self.label_pw.setFrameShadow(QtWidgets.QFrame.Sunken)
         self.label_pw.setAlignment(QtCore.Qt.AlignCenter)
         self.label_pw.setStyleSheet("background: rgb(180, 180, 180)")
         self.label_pw.setObjectName("label_pw")
         self.counter_1 = QtWidgets.QSpinBox(self.centralwidget)
-        self.counter_1.setGeometry(QtCore.QRect(160, 90, 40, 20))
+        self.counter_1.setGeometry(QtCore.QRect(120, 65, 40, 20))
         self.counter_1.setObjectName("counter_1")
         self.counter_1.setMinimum(int(1))
-        self.counter_1.setMaximum(int(24))
+        self.counter_1.setMaximum(int(256))
         self.counter_1.setAutoFillBackground(False)
         self.counter_1.setStyleSheet("background: rgb(186, 186, 186)")
         self.radioButton = QtWidgets.QRadioButton(self.centralwidget)
-        self.radioButton.setGeometry(QtCore.QRect(20, 60, 70, 20))
+        self.radioButton.setGeometry(QtCore.QRect(180, 65, 70, 20))
         palette = QtGui.QPalette()
         brush = QtGui.QBrush(QtGui.QColor(255, 255, 255))
         brush.setStyle(QtCore.Qt.SolidPattern)
         palette.setBrush(QtGui.QPalette.Active, QtGui.QPalette.WindowText, brush)
         palette.setBrush(QtGui.QPalette.Inactive, QtGui.QPalette.WindowText, brush)
         self.radioButton.setPalette(palette)
-        self.exitbtn = QtWidgets.QPushButton(self.centralwidget)
-        self.exitbtn.setGeometry(QtCore.QRect(208, 0, 12, 12))
-        self.exitbtn.clicked.connect(self.exitP)
-        palette.setBrush(QtGui.QPalette.Active, QtGui.QPalette.ButtonText, brush)
-        palette.setBrush(QtGui.QPalette.Inactive, QtGui.QPalette.ButtonText, brush)
-        brush.setStyle(QtCore.Qt.SolidPattern)
-        self.exitbtn.setPalette(palette)
-        self.exitbtn.setObjectName("exitbtn")
         self.pwlist = QtWidgets.QListWidget(self.centralwidget)
-        self.pwlist.setGeometry(QtCore.QRect(0, 120, 220, 160))
+        self.pwlist.setGeometry(QtCore.QRect(20, 100, 640, 360))
         palette.setBrush(QtGui.QPalette.Active, QtGui.QPalette.Text, brush)
         palette.setBrush(QtGui.QPalette.Inactive, QtGui.QPalette.Text, brush)
         self.pwlist.setPalette(palette)
@@ -64,12 +65,11 @@ class Ui_MainWindow(QWidget):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "000.2"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "000.4"))
         self.button_gen.setText(_translate("MainWindow", "GENERATE"))
         self.label_pw.setText(_translate("MainWindow", "Password"))
-        self.radioButton.setText(_translate("btn_restrict", "SYMBOLS"))
-        self.exitbtn.setText(_translate("MainWindow", "X"))
-        self.counter_1.setValue(24)
+        self.radioButton.setText(_translate("MainWindow", "SYMBOLS"))
+        self.counter_1.setValue(16)
 
     def generatePW(self):
         reynge = self.counter_1.value()
@@ -83,9 +83,6 @@ class Ui_MainWindow(QWidget):
         self.pwlist.addItem(pw)
         f = open("pw.txt", "a+")
         f.write(pw + "\n")
-
-    def exitP(self):
-        sys.exit(app.exec_())
 
 
 if __name__ == "__main__":
